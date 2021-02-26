26ce793bb0d53353d63ececdc82748d0e3b60c74
Benjamin Hughes <behu@kea.dk>
2021-02-26 13:49:17 +0100

    asd

---
+++ week-9/class-work/thymeleaf-continued/src/main/java/com/example/demo/controllers/KebabenhavnController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KebabenhavnController {
+    
+}

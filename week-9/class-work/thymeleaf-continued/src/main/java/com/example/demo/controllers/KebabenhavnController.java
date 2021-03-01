a05f50f1d33853a97b77429fe87d84cc7615adcb
Benjamin Hughes <behu@kea.dk>
2021-03-01 12:55:40 +0100

    asd

--- week-9/class-work/thymeleaf-continued/src/main/java/com/example/demo/controllers/KebabenhavnController.java
+++ week-9/class-work/thymeleaf-continued/src/main/java/com/example/demo/controllers/KebabenhavnController.java
@@ -1,9 +1,24 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class KebabenhavnController {
-    
+    @GetMapping("/")
+    public String renderHome(Model model) {
+        Boolean isUserLoggedIn = true;
+        model.addAttribute("isUserLoggedIn", isUserLoggedIn);
+
+        String emailProvider = "hotmail";
+        model.addAttribute("emailProvider", emailProvider);
+
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    public String renderAbout(Model model) {
+        return "about.html";
+    }
 }
